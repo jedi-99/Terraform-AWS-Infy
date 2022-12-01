8a95a9aab977407a2bc428633a72f537364c71a6
jedi-99 <96409539+jedi-99@users.noreply.github.com>
2022-12-01 19:08:15 +0530

    s3 config added

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "b" {
+  
+  bucket = "kfc-bucket-404"
+  force_destroy = false
+    
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  
+}
+
+resource "aws_s3_bucket_versioning" "versioning_config" {
+  bucket = aws_s3_bucket.b.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
